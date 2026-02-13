48c5012babcb6bb495b8acb1e2c5e3ca310205de
Gathsara2002 <liviru2002mmg@gmail.com>
2026-02-13 19:52:32 +0530

    inheritance 2nd program

---
+++ src/inheritance/InheritanceTwo.java
@@ -0,0 +1,29 @@
+package inheritance;
+
+/**
+ * @author : Gathsara
+ * created : 2/13/2026 -- 7:47 PM
+ **/
+
+public class InheritanceTwo {
+    public static void main(String[] args) {
+        // Create a new Adder object
+        Adder a = new Adder();
+
+        // Print the name of the superclass on a new line
+        System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());
+
+        // Print the result of 3 calls to Adder's `add(int,int)` method as 3 space-separated integers:
+        System.out.print(a.add(10, 32) + " " + a.add(10, 3) + " " + a.add(10, 10) + "\n");
+    }
+}
+
+class Arithmetic {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
+class Adder extends Arithmetic {
+
+}
